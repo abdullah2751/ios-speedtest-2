b213524df5c9f70427912aa5b2e4002c3d02f620
alhamad478 <70263707+alhamad478@users.noreply.github.com>
2020-09-16 17:23:46 +0300

    done

--- SPEED2.playground/Contents.swift
+++ SPEED2.playground/Contents.swift
@@ -8,9 +8,12 @@
 
 */
 var numbers = [1,2,3,4,5,6,7,8,9,10]
-
-
-
+var evenNumbers : [Int] = []
+for number in numbers {
+    if number % 2 == 0{
+        evenNumbers.append(number)
+    }
+}
 /*:
 
 # BONUS🎁 قم بكتابه كود يقوم بطباعة الأرقام الزوجية فقط 
